dfdab7d05b2e5467320c4e70156c909dfec058f0
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-01-26 16:36:06 -0300

    feat(Manager Accounts): Implementando VO de email com testes

---
+++ lalouise.backend/src/main/java/matheusfraga/dev/lalouise/backend/core/vo/UserEmail.java
@@ -0,0 +1,20 @@
+package matheusfraga.dev.lalouise.backend.core.vo;
+
+import matheusfraga.dev.lalouise.backend.core.exception.DomainException;
+
+import java.util.regex.Pattern;
+
+public record UserEmail(String value) {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public UserEmail{
+        if (value == null || value.isBlank()) throw new DomainException("Value cant be null or blank");
+
+        value = value.trim().toLowerCase();
+
+        if (!EMAIL_PATTERN.matcher(value).matches()) throw new DomainException("Invalid email address");
+    }
+
+}
---
+++ lalouise.backend/src/test/java/matheusfraga/dev/lalouise/backend/vo/UserEmailVoTest.java
@@ -0,0 +1,42 @@
+package matheusfraga.dev.lalouise.backend.vo;
+
+import matheusfraga.dev.lalouise.backend.core.exception.DomainException;
+import matheusfraga.dev.lalouise.backend.core.vo.UserEmail;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserEmailVoTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "contato@lalouise.com",
+            "suporte.tecnico@empresa.com.br",
+            "dev_123@gmail.io"
+    })
+    @DisplayName("Deve criar UserEmail com sucesso para endereços válidos")
+    void shouldCreateUserEmailWhenAddressIsValid(String validEmail) {
+        var email = new UserEmail(validEmail);
+
+        assertThat(email.value()).isEqualTo(validEmail.trim());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "email_sem_arroba.com",
+            "usuario@dominio",
+            "usuario@dominio.",
+            "usuario@dominio.c",
+            "   ",
+            "plainaddress"
+    })
+    @DisplayName("Deve lançar DomainException para endereços de e-mail inválidos")
+    void shouldThrowExceptionWhenAddressIsInvalid(String invalidEmail) {
+        assertThatThrownBy(() -> new UserEmail(invalidEmail))
+                .isInstanceOf(DomainException.class);
+
+    }
+}
