156c9670d0cc5d3ad003cefe782b80d56c8389d0
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-02-08 14:06:49 -0300

    feat/print-local-service

---
+++ lalouise.printerservice/src/main/java/matheusfragadev/br/com/lalouise/printerservice/Application.java
@@ -0,0 +1,13 @@
+package matheusfragadev.br.com.lalouise.printerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
