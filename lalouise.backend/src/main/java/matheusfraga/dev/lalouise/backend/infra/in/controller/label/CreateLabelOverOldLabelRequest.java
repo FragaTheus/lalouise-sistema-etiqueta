562e49f7189f7e83c539559b3c61ddbcb14b5893
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-02-10 15:56:21 -0300

    feat: Testando rabbit producer de forma intefrada e corrigindo algumas anotacoes do validation que estava atrapalhando o processamento de dados como UUID e Enuns

--- lalouise.backend/src/main/java/matheusfraga/dev/lalouise/backend/infra/in/controller/label/CreateLabelOverOldLabelRequest.java
+++ lalouise.backend/src/main/java/matheusfraga/dev/lalouise/backend/infra/in/controller/label/CreateLabelOverOldLabelRequest.java
@@ -2,12 +2,14 @@ package matheusfraga.dev.lalouise.backend.infra.in.controller.label;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import matheusfraga.dev.lalouise.backend.domain.enums.StorageType;
 
 import java.util.UUID;
 
 public record CreateLabelOverOldLabelRequest(
-        @NotBlank
+
+        @NotNull
         StorageType storageType,
 
         @Min(1)
--- lalouise.backend/src/main/java/matheusfraga/dev/lalouise/backend/infra/in/controller/label/CreateLabelRequest.java
+++ lalouise.backend/src/main/java/matheusfraga/dev/lalouise/backend/infra/in/controller/label/CreateLabelRequest.java
@@ -2,6 +2,7 @@ package matheusfraga.dev.lalouise.backend.infra.in.controller.label;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import matheusfraga.dev.lalouise.backend.domain.enums.StorageType;
 
@@ -10,10 +11,10 @@ import java.util.UUID;
 @Builder
 public record CreateLabelRequest(
 
-        @NotBlank
+        @NotNull
         UUID productId,
 
-        @NotBlank
+        @NotNull
         StorageType storageType,
 
         @Min(1)
