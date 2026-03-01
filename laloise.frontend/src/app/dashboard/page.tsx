f1a4dbe22167d3ea26f8e323d6de6a58ce77057d
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-03-01 17:46:21 -0300

    feat: Implementando dash

--- laloise.frontend/src/app/dashboard/page.tsx
+++ laloise.frontend/src/app/dashboard/page.tsx
@@ -1,9 +1,65 @@
-import AppHeader from "@/components/app-header";
+import AppDashboardLayout from "@/components/app-dashboard-layout";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import userIcon from "@/assets/user-icon.svg";
+import { dashboardCardProps } from "@/constants/dashboard-cards-props";
 
-export default function Dashboard() {
+export interface DashboardCardProps {
+  srcUrl: string;
+  link: string;
+  title: string;
+  description: string;
+}
+
+export default function Dashboard({
+  srcUrl,
+  link,
+  title,
+  description,
+}: DashboardCardProps) {
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <AppDashboardLayout>
+      <div className="flex-1 grid grid-cols-1 auto-rows-auto lg:grid-cols-3 h-full gap-4 lg:gap-8">
+        {dashboardCardProps.map((cards, index) => (
+          <Card key={index}>
+            <CardContent className="items-center justify-center flex">
+              <Image
+                src={cards.srcUrl}
+                alt="Icone de usuario"
+                className="w-full max-w-xs pointer-events-none"
+              />
+            </CardContent>
+
+            <CardHeader className="order-first lg:order-2">
+              <CardTitle>{cards.title}</CardTitle>
+              <CardDescription>{cards.description}</CardDescription>
+            </CardHeader>
+
+            <CardFooter className="order-last">
+              <Link href={cards.link} className="w-full">
+                <Button
+                  variant={"outline"}
+                  className="flex items-center w-full group"
+                >
+                  <span>Gerenciar</span>
+                  <ChevronRight className="group-hover:ml-1 duration-300 transition-all " />
+                </Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </AppDashboardLayout>
   );
 }
