f8e4a95b675d7acb46371324b77395bcb8445b66
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2026-03-15 01:11:46 -0300

    feat: Subindo para server o backend e mudadno endereco aqui

--- laloise.frontend/src/app/painel/etiquetas/[id]/page.tsx
+++ laloise.frontend/src/app/painel/etiquetas/[id]/page.tsx
@@ -1,5 +1,5 @@
 import DetailsLabelPageLayout from "@/features/label/layouts/details-label-page-layout";
 
 export default function LabelInfoPage() {
-	return <DetailsLabelPageLayout />;
+  return <DetailsLabelPageLayout />;
 }
--- laloise.frontend/src/config/http/api.ts
+++ laloise.frontend/src/config/http/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError } from "axios";
 import { HandlerResponse } from "./api.error";
 
-const API_BASE_URL = "http://localhost:8080/api/v1";
+//const API_BASE_URL = "http://localhost:8080/api/v1";
+const API_BASE_URL = "https://lalouise-backend-latest.onrender.com/api/v1";
 
 
 
